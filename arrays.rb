0d72e9f4fbfe4eb7d3cb423b03ad549f97d515ad
Bradley Milton <bmilton2412@gmail.com>
2023-02-22 19:29:34 -0800

    Show 4 methods and their definitions

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,12 @@ Ages = [32, 33, 31, 28]
 version_number = [122.0, 122.1, 122.2, 122.3]
 updated_version_number = [True, True, False, True]
 
+#pop will remove an element to the end of the array.
+Name.pop 
+#push adds an element to the end of the array.
+ages.push(35)
+#shift removes the first element in an array.
+version_number.shift
+#unshift adds an element to the front of an array.
+variable.unshift(False)
+
